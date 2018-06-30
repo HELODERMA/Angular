8289baf783b3af16808205a697233a7430acdf06
Heloderma <sjosue43@gmail.com>
2018-06-29 21:03:10 -0500

    Upadating changes

---
+++ Ejercicio3/js/empresarios.js
@@ -0,0 +1,6 @@
+var profesorData = {
+  nombre: "Josué Sandoval Sánchez",
+  bio: "Empedernido soñador, amante de la naturaleza, programador y artista",
+  edad: 23;
+  foto: "img/jos.jpg"
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AngularJS
 Prácticas conglomeradas de angular js
+
+Ejercicio 1:
+Ejercicio 1:
+Ejercicio 3: Carga Dinamica de Datos
+ 
