22fb2736a96dd2a6586a2652e0d3dc92c8ad1025
HELODERMA <sjosue43@gmail.com>
2018-07-05 11:06:36 -0500

    ultimos cambios

---
+++ Ejercicio4/js/test.js
@@ -0,0 +1,24 @@
+{
+    "productos": [
+        {
+            "numerodoc": "211",
+            "tatalViaje": "MX212S8169",
+            "Client number": "100",
+            "preventaid": "28340A8167",
+            "id": "0012330552        COFFEE-MATE ORIGINAL Tira17(10x34g)N1MX ",
+            "checkout": "10",
+            "asigPedidos": "10",
+            "extra": "0",
+            "importe": "14704.24"
+        },
+        {
+            "Client number": "111",
+            "preventaid": "28340D8167",
+            "id": "0012352996        NESCAFE CLASICO Stick MP 6(15x14g) MX   ",
+            "checkout": "1",
+            "asigPedidos": "1",
+            "extra": "0"
+        },
+
+    ]
+}
