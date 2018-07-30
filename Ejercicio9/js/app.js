dec6bd2d1c875c48b72b659a1ec8f9b107eac29a
Heloderma <sjosue43@gmail.com>
2018-07-30 18:51:14 -0500

    JSONP working always

--- Ejercicio9/js/app.js
+++ Ejercicio9/js/app.js
@@ -2,40 +2,31 @@
 
 var app = angular.module('myApp',[ ]);
 
-
-//Si comento el bloque app.config, me sale error $sce:insecurl por la petición de geoplugin
-// app.config(function($sceDelegateProvider) {
-//     $sceDelegateProvider.resourceUrlWhitelist([
-// 				'self',
-//         'http://www.geoplugin.net/**'
-//     ]);
-// });
-
-app.config(function ($sceDelegateProvider) {
- $sceDelegateProvider.resourceUrlWhitelist(['http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&callback=JSON_CALLBACK&appid=9f50a805aa0089a1edd1829a5db029f0']);
-})
-
+app.config(function($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+				'http://api.openweathermap.org/data/2.5/weather?**'
+    ]);
+});
 
 
 app.controller('mainCtrl', ['$scope','$http','$sce', function($scope,$http,$sce){
 
-    // $scope.geo = {};
-	  // let a = $http.jsonp('http://api.openweathermap.org/', {jsonpCallbackParam: 'data/2.5/weather?lat=35&lon=139&callback=JSON_CALLBACK&appid=9f50a805aa0089a1edd1829a5db029f0'})
-	  //       .then(function(data){
-	  //        console.log(data.data);
-	  //       })
-	  //       .catch( function(error){
-	  //         console.warn( error );
-	  //       })
+    $scope.clima = {};
 
-					$http.jsonp('http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&&appid=9f50a805aa0089a1edd1829a5db029f0')
-				 .then(function sucecess(data) {
-				 console.log("Todo bien.");
-				 console.log(data);
-				 }, function error(response) {
-				 console.log("Todo mal.");
-				 console.log(response);
-				 })
+		var params = {
+			lat: 35,
+			lon: 139,
+			appid: '9f50a805aa0089a1edd1829a5db029f0'
+    };
+
+		$http.jsonp('http://api.openweathermap.org/data/2.5/weather?', { params: params })
+				        .then(function(data){
+								 $scope.clima = data.data;
+				         console.log(data.data);
+				        })
+				        .catch( function(error){
+				          console.warn( error );
+				        })
 
 }]);
 })();
